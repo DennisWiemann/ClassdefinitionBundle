3334067afc8ddbed059a8b96ff24cd0daf14c705
Dennis Wiemann <de95wi@googlemail.com>
2023-01-13 19:47:00 +0100

    changed namespace and name

--- src/ClassdefinitionBundle.php
+++ src/ClassdefinitionBundle.php
@@ -2,7 +2,7 @@
 
 namespace Classdefinition;
 
-use DennisWiemann\ClassdefinitionBundle\DependencyInjection\ClassdefinitionExtension;
+use Classdefinition\DependencyInjection\ClassdefinitionExtension;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class ClassdefinitionBundle extends AbstractBundle
